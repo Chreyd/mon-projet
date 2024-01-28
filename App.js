91dbd5a6084e4492de4ddb405c910f9a8857abcf
Fleudry <chreydmapeba@gmail.com>
2024-01-28 23:59:01 +0100

    Exercice 10: keyExtractor

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { StyleSheet, View, Text, FlatList } from "react-native";
 
 export default function App() {
   const obj = [
-    { id: "1", name: "Stan", age: 45 },
-    { id: "2", name: "Francine", age: 45 },
-    { id: "3", name: "hayley", age: 18 },
-    { id: "4", name: "Steve", age: 14 },
-    { id: "5", name: "Roger", age: 1020 },
-    { id: "6", name: "Klaus", age: 30 },
+    { name: "Stan", age: 45 },
+    { name: "Francine", age: 45 },
+    { name: "hayley", age: 18 },
+    { name: "Steve", age: 14 },
+    { name: "Roger", age: 1020 },
+    { name: "Klaus", age: 30 },
   ];
 
   const [family, setFamily] = useState(obj);
@@ -25,8 +25,8 @@ export default function App() {
     <View style={styles.container}>
       <FlatList
         data={family}
-        renderItem={( {item} ) => {
-          console.log(item);
+        renderItem={({ item }) => {
+          // console.log(item);
           return (
             <View style={styles.list}>
               <Text style={styles.textList}>
@@ -35,7 +35,7 @@ export default function App() {
             </View>
           );
         }}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item,index) => index.toString()}
       />
 
       {/*       <ScrollView
